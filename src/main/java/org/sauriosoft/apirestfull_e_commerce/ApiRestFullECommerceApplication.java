53d112814829fe000ef263b588d4951845f165d7
TigselemaAlex <84351430+TigselemaAlex@users.noreply.github.com>
2022-06-15 13:20:28 -0500

    Proyecto inicial

---
+++ src/main/java/org/sauriosoft/apirestfull_e_commerce/ApiRestFullECommerceApplication.java
@@ -0,0 +1,13 @@
+package org.sauriosoft.apirestfull_e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestFullECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestFullECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sauriosoft/apirestfull_e_commerce/ApiRestFullECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sauriosoft.apirestfull_e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestFullECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
